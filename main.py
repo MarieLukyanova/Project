93052d78d669a17695e2612e210bb2239b12889b
Maria <sdfghj>
2022-04-15 23:25:01 +0300

    хостинг

--- main.py
+++ main.py
@@ -178,7 +178,8 @@ def add_photo():
 
 def main():
     db_session.global_init('db/life_of_party.sqlite')
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
